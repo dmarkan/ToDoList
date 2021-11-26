5cf832b01bbc3be976981ccadce810bf1fb62824
Darko Markan <75615049+dmarkan@users.noreply.github.com>
2021-11-26 12:00:02 +0100

    created date.js and refactored the code

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set('view engine', 'ejs');
 
@@ -11,28 +12,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    let today = new Date();
-  
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    }
-
-    app.post("/", function (req, res) {
-        let item = req.body.newItem;
-
-        items.push(item);
-        
-        res.redirect("/")
-        
-    })
-
-    let day = today.toLocaleDateString("en-US", options);
-
+    const day = date.getDate();
     res.render('list', {
         kindOfDay: day, newListItems: items
     });
+
+    app.post("/", function (req, res) {
+        const item = req.body.newItem;
+        items.push(item);      
+        res.redirect("/")  
+    })
 })
 
 app.listen(3000, function () {
